e89789b084cde89affdc27f9ef2c38ceecd02be6
Manjunadh <manjunadhb@gmail.com>
2021-07-16 16:29:02 +0530

    Adding files to global repository.

---
+++ Pulse/Pulse/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Pulse
+//
+//  Created by Manjunadh Bommisetty on 16/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        var age:UInt8 = 20
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
